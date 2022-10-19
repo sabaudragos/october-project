548b9de3c7784a604eac69b720a97b58d787486a
Dragos Sabau <sabau.dragos@gmail.com>
2022-10-19 12:43:10 +0300

    live coding session 1 - company add (#1)
    * live coding session 1 - company add
    
    * Addressed review comments

---
+++ src/app/model/company.ts
@@ -0,0 +1,13 @@
+export class Company {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  country: string;
+  email: string;
+  phone: string;
+  registrationNumber: number;
+  vatNumber: string;
+}
+
+
